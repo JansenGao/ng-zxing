4209d0e7041b90455dab2e5dd5ade4e8c1d830e5
Gao, Jansen-JX <Jansen-JX.Gao@aia.com>
2019-12-16 14:25:22 +0800

    Rename repo name.

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('zxing-app app is running!');
+    expect(page.getTitleText()).toEqual('ng-zxing app is running!');
   });
 
   afterEach(async () => {
