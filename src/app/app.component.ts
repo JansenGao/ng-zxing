f0c27b92790ef54f901297c743b0a1c83f44683f
Gao, Jansen-JX <Jansen-JX.Gao@aia.com>
2019-12-16 15:16:00 +0800

    Remove items.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Output, Input, EventEmitter } from '@angular/core';
+import { BrowserCodeReader, BrowserQRCodeReader } from '@zxing/library';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ng-zxing';
+  codeReader: BrowserCodeReader = new BrowserQRCodeReader();
+  deviceList: Array<any>;
+  selectedDeviceId;
+
+  constructor(){}
+
+  ngOnInit(){
+    this.codeReader.getVideoInputDevices()
+    .then((videoInputDevices) => {
+      this.deviceList = videoInputDevices;
+      this.selectedDeviceId = this.deviceList[0].deviceId;
+
+    })
+    .catch((err) => {
+      console.log('qrcode init error:'+ err);
+    });
+
+    this.startScan();
+  }
+
+  startScan(){
+    this.codeReader.decodeFromInputVideoDevice(this.selectedDeviceId, 'video')
+    .then(
+      result => {
+        console.log(result);
+      }
+    );
+  }
 }
